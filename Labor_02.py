2eb4930ff15dbcbfbfe877e6cb046e6d96e52658
KIgor118 <kerdiigor@gmail.com>
2024-09-12 10:24:52 +0200

    2

--- Labor_02.py
+++ Labor_02.py
@@ -18,5 +18,17 @@ print(f"Szia {felhasznalo_neve}")
 
 print("neve: {0} ,\nkora: {1}".format(felhasznalo_kora,felhasznalo_kora) )
 
-print(felhasznalo_neve.rjust(50))
+print(felhasznalo_neve.rjust(50,"."))
+print(str(felhasznalo_kora).rjust(50,"."))
 
+print(felhasznalo_neve.ljust(50,"."))
+print(str(felhasznalo_kora).ljust(50,"."))
+
+print(felhasznalo_neve.center(50,"."))
+print(str(felhasznalo_kora).center(50,"."))
+
+felhasznalo_neve=input("\nKérem a nevét: ")
+felhasznalo_kora=input("Hány éves vagy: ")
+
+print("\nSzia",felhasznalo_neve)
+print("Biztosan ",felhasznalo_kora, " éves vagy? Nem", int(felhasznalo_kora) + 10)
