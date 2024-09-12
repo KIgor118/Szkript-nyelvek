146df07ea6a4f97ef151af50f3f168640a890459
KIgor118 <kerdiigor@gmail.com>
2024-09-12 10:05:05 +0200

    elso

---
+++ Labor_02.py
@@ -0,0 +1,22 @@
+print ("Szia DUE!",end='')
+
+print("\tSzia", "DUE!")
+
+print("Szia","DUE!",sep="-")
+
+print("Szia","\n\tDUE!")
+
+print('''Szép napounk
+lesz ma!
+''')
+
+felhasznalo_neve= "Józsi"
+felhasznalo_kora= 21
+
+print("Szia",felhasznalo_neve)
+print(f"Szia {felhasznalo_neve}")
+
+print("neve: {0} ,\nkora: {1}".format(felhasznalo_kora,felhasznalo_kora) )
+
+print(felhasznalo_neve.rjust(50))
+
